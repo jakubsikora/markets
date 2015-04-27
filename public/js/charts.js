c7ae5293e1ee51d4acfcada74b0c9ba7dde07da4
Jakub Sikora <jakub.sikora@pure360.com>
2015-04-27 13:28:30 +0100

    wip

--- public/js/charts.js
+++ public/js/charts.js
@@ -1,51 +1,81 @@
 $(function () {
-  Highcharts.setOptions({
-      global : {
-          useUTC : false
-      }
-  });
-
-  // Create the chart
-  $('#container').highcharts('StockChart', {
-      chart : {
-          events : {
-              load : function () {
-                // set up the updating of the chart each second
-                var series = this.series[0];
-                console.log(series);
-                setInterval(function () {
-                    var x = markets[0].timestamp,
-                        y = parseFloat(markets[0].ask);
-
-                    console.log(x, y);
-
-                    series.addPoint([x, y], true, true);
-                }, 1000);
-              }
-          }
-      },
-
-      rangeSelector: {
-          selected: 1
-      },
-
-      title : {
-          text : 'Live Markets data'
-      },
-
-      exporting: {
-          enabled: false
-      },
-
-      series : [{
-        name : 'Data',
-        data : [
-          [1429983167319,1.2333],
-          [1429983720015, 1.250348]
-        ],
-        tooltip: {
-           valueDecimals: 6
+    Highcharts.setOptions({
+        global : {
+            useUTC : false
         }
-      }]
-  });
+    });
+
+    // Create the chart
+    $('#chart').highcharts('StockChart', {
+        chart : {
+            events : {
+                load : function () {
+                  var that = this;
+                  // set up the updating of the chart each second
+                  var bidSeries = this.series[0];
+                  var askSeries = this.series[1];
+
+                  setInterval(function() {
+                    markets.forEach(function(market) {
+                      // that.addSeries({
+                      //   name: market.id,
+                      //   data: []
+                      // });
+                      bidSeries.addPoint([(new Date).getTime(), Number(market.bid)], true, false);
+                      askSeries.addPoint([(new Date).getTime(), Number(market.ask)], true, false);
+                    });
+                  }, 2000);
+
+                  // window.subscribeToMarket('eurusd', function(tick) {
+                  //     var bid = null;
+                  //     var ask = null;
+                  //     if (tick.bid !== 'undefined') {
+                  //         bid = Number(parseFloat(tick.bid).toFixed(6));
+                  //         bidSeries.addPoint([tick.time, bid], true, false);
+                  //     }
+
+                  //     // if (tick.ask !== 'undefined') {
+                  //     //     ask = Number(parseFloat(tick.ask).toFixed(6));
+                  //     //     askSeries.addPoint([tick.time, ask], true, false);
+                  //     // }
+                  // });
+
+                }
+            }
+        },
+
+        rangeSelector: {
+            buttons: [{
+                count: 1,
+                type: 'minute',
+                text: '1M'
+            }, {
+                count: 5,
+                type: 'minute',
+                text: '5M'
+            }, {
+                type: 'all',
+                text: 'All'
+            }],
+            inputEnabled: false,
+            selected: 0
+        },
+
+        title : {
+            text : 'Live market data'
+        },
+
+        exporting: {
+            enabled: false
+        },
+
+        series : [{
+            name : 'Bid',
+            data : [ ],
+        },{
+            name : 'Ask',
+            data : [ ],
+        }]
+    });
+
 });
